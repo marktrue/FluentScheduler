0d41887682fd93b03044d8166293ba30ad5adfc0
marktrue <ahu.marktrue@gmail.com>
2017-09-14 00:15:31 +0800

    add XmlConfigurator

---
+++ Library/Config/XmlConfigurator.cs
@@ -0,0 +1,13 @@
+﻿namespace FluentScheduler.Config
+{
+    using System;
+    using System.Xml;
+    using System.IO;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class XmlConfigurator
+    {
+    }
+}
